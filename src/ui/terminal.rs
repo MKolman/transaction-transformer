11c1d6581a771305127079919df23ab784952705
Maks Kolman <maks@kolman.si>
2022-02-06 22:53:21 +0100

    Group candidates by target account when prompting user

--- src/ui/terminal.rs
+++ src/ui/terminal.rs
@@ -1,22 +1,40 @@
+use std::collections::HashMap;
 use std::io::Write;
 
 use crate::matcher::Candidate;
+
+type GroupedCandidates<'a, 'b> = (String, Vec<&'a Candidate<'b>>);
+
 pub struct UI;
 
 impl UI {
-    fn ask_user_to_pick(account: &str, candidates: &[Candidate]) -> String {
+    fn group_candidates<'a, 'b>(candidates: &'a [Candidate<'b>]) -> Vec<GroupedCandidates<'a, 'b>> {
+        let mut grouped: HashMap<String, Vec<_>> = HashMap::new();
+        for candidate in candidates {
+            grouped
+                .entry(candidate.translates_to.clone())
+                .or_default()
+                .push(candidate);
+        }
+        grouped
+            .values_mut()
+            .for_each(|candidates| candidates.sort_unstable());
+        grouped.into_iter().collect()
+    }
+    fn ask_user_to_pick(account: &str, candidates: &[GroupedCandidates]) -> String {
         println!("\n=== {account} ===", account = account);
         if candidates.is_empty() {
             print!("Enter account: ");
         } else {
-            for (i, candidate) in candidates.iter().enumerate() {
-                println!(
-                    "{i}. {score:.1}% ({to}) {from}",
-                    i = i + 1,
-                    score = candidate.score * 100.0,
-                    to = candidate.translates_to,
-                    from = candidate.similar_to,
-                );
+            for (i, (account, candidates)) in candidates.iter().enumerate() {
+                println!("{i}. {account}", i = i + 1, account = account,);
+                for candidate in candidates {
+                    println!(
+                        "\t{score:.1}% {from}",
+                        score = candidate.score * 100.0,
+                        from = candidate.similar_to,
+                    );
+                }
             }
             print!("[1-{len}] or new: ", len = candidates.len());
         }
@@ -28,11 +46,10 @@ impl UI {
 }
 impl super::UI for UI {
     fn choose_or_create_match(&self, account: &str, candidates: &[Candidate]) -> String {
-        let value = Self::ask_user_to_pick(account, candidates);
+        let candidates = Self::group_candidates(candidates);
+        let value = Self::ask_user_to_pick(account, &candidates);
         match value.parse::<usize>() {
-            Ok(idx) if 0 < idx && idx <= candidates.len() => {
-                candidates[idx - 1].translates_to.clone()
-            }
+            Ok(idx) if 0 < idx && idx <= candidates.len() => candidates[idx - 1].0.clone(),
             _ => value,
         }
     }
